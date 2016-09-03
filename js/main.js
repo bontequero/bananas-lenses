aaf423b33b5cae55a428bce05b4aea56058e3125
bontequero <bontequero@gmail.com>
2016-09-03 17:13:14 +0300

    add <download all> button

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ var App = new Vue({
 
   YahooLib.fetchJSON(FEED_URL);
 }());
+
+function downloadURI(uri) {
+  var link = document.createElement("a");
+  link.download = '';
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
+(function () {
+  let down = document.getElementById('down');
+
+  down.addEventListener('click', () => {
+    App.podcasts.forEach((item) => {
+      downloadURI(item.enclosure.url);
+    });
+  });
+})();
